0332bb492f392e3a0e746bf768ffb0d4e013226f
Alexander Ryzen WSL <alexander9306@gmail.com>
2020-12-06 13:42:55 -0400

    added enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,23 @@ import { join } from "path";
 import { ConfigModule } from "@nestjs/config";
 import { EventsModule } from "./events/events.module";
 
+const isDevelopment = () => process.env.NODE_ENV === "development";
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: isDevelopment() ? ".env.development" : ".env.production",
+    }),
     TypeOrmModule.forRoot({
       type: "sqlite",
       database: join(process.cwd(), "database/data.sqlite3"),
       autoLoadEntities: true,
-      synchronize: process.env.NODE_ENV === "development",
+      synchronize: isDevelopment(),
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), "src/schema.graphql"),
       debug: false,
-      playground: process.env.NODE_ENV === "development",
+      playground: isDevelopment(),
     }),
     EventsModule,
   ],
